961fa3249bc04fb1926df9fdcbb24a0b5cf6cc63
Florian Vivet <florian.vivet@etu.unilim.fr>
2021-02-22 13:53:46 +0100

    tests barre compétences

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,13 @@ $(function(){
             if($("body").css("background-color")=="rgb(255, 255, 255)"){
 
               $("body").css("background-color","#1f1f1f");
-              $(".dark").css({"background-color":"#333333", "color": "white"});
+              $(".dark").css({"background-color":"#333333", "color": "white", "border-color":"white"});
               $("#darkmode").attr("src", "images/lightmode.png");
 
             }else{
 
               $("body").css("background-color","#ffffff");
-              $(".dark").css({"background-color":"#ffffff", "color": "black"});
+              $(".dark").css({"background-color":"#ffffff", "color": "black", "border-color":"black"});
               $("#darkmode").attr("src", "images/darkmode.png");
 
             }
